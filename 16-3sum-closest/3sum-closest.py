80c5cc398ef7645c34582f5fe72dc29c0a200492
Esaf Shanum <esafshanum@gmail.com>
2026-07-26 23:36:59 +0530

    Time: 1003 ms (5.02%) | Memory: 12.4 MB (41.68%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -5,20 +5,26 @@ class Solution(object):
         closest = nums[0] + nums[1] + nums[2]
 
         for i in range(n - 2):
-            left = i + 1
-            right = n - 1
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, n - 1
 
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
 
-                if abs(target - total) < abs(target - closest):
+                if abs(total - target) < abs(closest - target):
                     closest = total
 
-                if total < target:
-                    left += 1
-                elif total > target:
-                    right -= 1
-                else:
+                if total == target:
                     return target
+                elif total < target:
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                else:
+                    right -= 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
 
         return closest
