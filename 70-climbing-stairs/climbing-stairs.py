ee3287c60640539b9ed75b6d598435d70f07048a
Esaf Shanum <esafshanum@gmail.com>
2026-07-27 23:41:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (57.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,27 +1,11 @@
 class Solution(object):
     def climbStairs(self, n):
-        def multiply(a, b):
-            return [
-                [
-                    a[0][0] * b[0][0] + a[0][1] * b[1][0],
-                    a[0][0] * b[0][1] + a[0][1] * b[1][1]
-                ],
-                [
-                    a[1][0] * b[0][0] + a[1][1] * b[1][0],
-                    a[1][0] * b[0][1] + a[1][1] * b[1][1]
-                ]
-            ]
-
-        def power(mat, p):
-            result = [[1, 0], [0, 1]]
-            while p:
-                if p & 1:
-                    result = multiply(result, mat)
-                mat = multiply(mat, mat)
-                p >>= 1
-            return result
-
         if n <= 2:
             return n
 
-        return power([[1, 1], [1, 0]], n)[0][0]
+        first, second = 1, 2
+
+        for _ in range(3, n + 1):
+            first, second = second, first + second
+
+        return second
